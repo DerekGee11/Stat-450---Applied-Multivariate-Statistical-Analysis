d67d345c3600477b094cf06ea26c39794108b123
DerekGee11 <156268872+DerekGee11@users.noreply.github.com>
2024-01-16 13:10:49 -0800

    Stat 450 Project R Code.R

--- Stat 450 Project R Code.R
+++ Stat 450 Project R Code.R
@@ -1,5 +1,5 @@
 # Stat-450---Applied-Multivariate-Statistical-Analysis
-Real Estate Analysis
+# Real Estate Analysis
 
 ```{r｝
 dfuncleaned <- read_csv("real_estateUncleaned.csv")
@@ -11,17 +11,17 @@ sum(is.na(dfuncleaned))
 
 ``` {r}
 df1 <- real_estate %>% as_tibble() %>%
-select-c(Alley, Fence, MiscFeature, PoolArea, PoolQC, Utilities, Street, FireplaceQu))
+select-c(Alley, Fence, MiscFeature, PoolArea, PoolQC, Utilities, Street, FireplaceQu)
 
 ```
 
 ``` {r}
-df1$LotFrontage[is.na(df1$LotFrontage)]<-mean(df1$LotFrontage,na.rm=TRUE
+df1$LotFrontage[is.na(df1$LotFrontage)]<-mean(df1$LotFrontage,na.rm=TRUE)
 
 ```
 
 ``` {r}
-df1 <- na.omit (df1)
+df1 <- na.omit(df1)
 
 ```
 
@@ -31,16 +31,16 @@ df1 <- read_csv("real_estatecleaned.csv")
 ```
 
 ``` {r}
-ggplot (df1, aes(x = YrSold, y = SalePrice)) + geom_point ()
+ggplot(df1, aes(x = YrSold, y = SalePrice)) + geom_point ()
 
 ```
 
 ``` {r}
 
 # Subset only numeric column
-df1_num <- select_if (df1, is. numeric)
-d ‹- df_num (, - 25]
-RS.pca <- princomp (d, cor = TRUE, scores=TRUE)
-summary (RS. pca)
+df1_num <- select_if(df1, is. numeric)
+d ‹- df_num(, - 25)
+RS.pca <- princomp(d, cor = TRUE, scores=TRUE)
+summary(RS. pca)
 
 ```
